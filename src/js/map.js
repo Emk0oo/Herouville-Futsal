7645e2cccba74496bcf5e7b8a8d7869251c7eb63
Emk0oo <emir.j2002@gmail.com>
2024-03-27 23:08:13 +0100

    [ADD] div1 & wave

--- src/js/map.js
+++ src/js/map.js
@@ -1,2 +1,10 @@
+var map = L.map('map').setView([49.202548542168834, -0.3442810549249789], 13);
+var marker = L.marker([49.202548542168834, -0.3442810549249789]).addTo(map);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
---
+++ src/js/swiper.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper', {
+      speed: 400,
+      spaceBetween: 100,
+      // Add other options as needed
+    });
+  });
+
+//   const swiper = document.querySelector('.swiper').swiper;
+
+// // Now you can use all slider methods like
+// swiper.slideNext();
